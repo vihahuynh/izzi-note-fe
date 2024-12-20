f419e68255af48e7ad50f4dc4308430137b598a1
vihahuynh <huynhviha1703@gmail.com>
2024-12-20 11:25:04 +0700

    feat: logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,24 @@
-import BurgerButton from './burgerButton/burgerButton'
-import classes from './header.module.scss'
+import BurgerButton from "./burgerButton/burgerButton";
+import classes from "./header.module.scss";
+import logo from "./../../assets/images/logo.jpg";
 
 const Header = () => {
-    return <div className={classes.header}>
-        <BurgerButton />
-        <div>logo</div>
-        <input />
-        <div>tools</div>
-        <div>user</div>
-    </div>
-}
+    return (
+        <div className={classes.header}>
+            <div className={classes.header__left}>
+                <BurgerButton />
+                <div className={classes.logo}>
+                    <img src={logo} width={40} />
+                    <span>IZZI</span>
+                </div>
+                <input />
+            </div>
+            <div className={classes.header__right}>
+                <div>tools</div>
+                <div>user</div>
+            </div>
+        </div>
+    );
+};
 
-export default Header
+export default Header;
